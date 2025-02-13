1f15969ca9800ba554fc506b0c8852bcec627257
Erik-Bagdasarov <124051964+Erik-Bagdasarov@users.noreply.github.com>
2025-02-13 15:50:12 +0500

    add singular test

--- models/events_stat.sql
+++ models/events_stat.sql
@@ -2,4 +2,4 @@ select
     count("type" = 'cancel_search' or null)
         / cast(count("type" = 'start_search' or null) as float)
         * 100 as cancel_pct
-from {{ ref('events_full') }}
+from {{ ref("events_full") }}
